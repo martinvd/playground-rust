8cdb29b06296483eebd80802caa5942c6742c132
Martin van Driel <martinvd@gmail.com>
2019-01-08 00:53:28 +0100

    Ignore invalid input

--- 200_guessing_game/src/main.rs
+++ 200_guessing_game/src/main.rs
@@ -20,10 +20,10 @@ fn main() {
       .read_line(&mut guess)
       .expect("Failed to read line");
 
-    let guess: u32 = guess
-      .trim()
-      .parse()
-      .expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
     println!("You guessed: {}", guess);
 
