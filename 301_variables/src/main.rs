03ef2d36b832e09062b9dbeb23f622f6cd484449
Martin van Driel <martinvd@gmail.com>
2019-01-09 08:20:37 +0100

    Experiment with shadowing

--- 301_variables/src/main.rs
+++ 301_variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-  let mut x = 5;
+  let mut x = "100";
   println!("The value of x is: {}", x);
-  x = 6;
+
+  x = "200";
+  println!("The value of x is: {}", x);
+
+  let x = x.len();
   println!("The value of x is: {}", x);
 }
